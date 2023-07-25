b6ace0da740f79c97c60a3d4b579e733be6c283e
Sergey Ershov <ershovu@gmail.com>
2023-07-25 20:19:43 +0300

    Create clean-docker.v2.sh

---
+++ clean-docker.v2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# /usr/local/clean-docker.sh
+
+# Remove all randomly named containers
+docker ps -aq --filter 'status=exited' --filter 'name=/[a-z]+_[a-z]+$' | xargs docker rm
+
+# Remove all dangling images, volumes and networks
+docker image prune -f
+docker volume prune -f
+docker network prune -f
